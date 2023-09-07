8961225ea8a9ae8282dda59ff3984df0e6811641
Nikita Koss <kossnikita@gmail.com>
2023-09-07 09:05:16 +0300

    add CRM CFG enums

--- src/at32wb415/crm/cfg.rs
+++ src/at32wb415/crm/cfg.rs
@@ -3,23 +3,378 @@ pub type R = crate::R<CFG_SPEC>;
 #[doc = "Register `CFG` writer"]
 pub type W = crate::W<CFG_SPEC>;
 #[doc = "Field `SCLKSEL` reader - System clock select"]
-pub type SCLKSEL_R = crate::FieldReader;
+pub type SCLKSEL_R = crate::FieldReader<SCLKSEL_A>;
+#[doc = "System clock select\n\nValue on reset: 0"]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+#[repr(u8)]
+pub enum SCLKSEL_A {
+    #[doc = "0: System clock from HICK"]
+    Hick = 0,
+    #[doc = "1: System clock from HEXT"]
+    Hext = 1,
+    #[doc = "2: System clock from PLL"]
+    Pll = 2,
+}
+impl From<SCLKSEL_A> for u8 {
+    #[inline(always)]
+    fn from(variant: SCLKSEL_A) -> Self {
+        variant as _
+    }
+}
+impl crate::FieldSpec for SCLKSEL_A {
+    type Ux = u8;
+}
+impl SCLKSEL_R {
+    #[doc = "Get enumerated values variant"]
+    #[inline(always)]
+    pub fn variant(&self) -> Option<SCLKSEL_A> {
+        match self.bits {
+            0 => Some(SCLKSEL_A::Hick),
+            1 => Some(SCLKSEL_A::Hext),
+            2 => Some(SCLKSEL_A::Pll),
+            _ => None,
+        }
+    }
+    #[doc = "System clock from HICK"]
+    #[inline(always)]
+    pub fn is_hick(&self) -> bool {
+        *self == SCLKSEL_A::Hick
+    }
+    #[doc = "System clock from HEXT"]
+    #[inline(always)]
+    pub fn is_hext(&self) -> bool {
+        *self == SCLKSEL_A::Hext
+    }
+    #[doc = "System clock from PLL"]
+    #[inline(always)]
+    pub fn is_pll(&self) -> bool {
+        *self == SCLKSEL_A::Pll
+    }
+}
 #[doc = "Field `SCLKSEL` writer - System clock select"]
-pub type SCLKSEL_W<'a, REG, const O: u8> = crate::FieldWriter<'a, REG, 2, O>;
+pub type SCLKSEL_W<'a, REG, const O: u8> = crate::FieldWriter<'a, REG, 2, O, SCLKSEL_A>;
+impl<'a, REG, const O: u8> SCLKSEL_W<'a, REG, O>
+where
+    REG: crate::Writable + crate::RegisterSpec,
+    REG::Ux: From<u8>,
+{
+    #[doc = "System clock from HICK"]
+    #[inline(always)]
+    pub fn hick(self) -> &'a mut crate::W<REG> {
+        self.variant(SCLKSEL_A::Hick)
+    }
+    #[doc = "System clock from HEXT"]
+    #[inline(always)]
+    pub fn hext(self) -> &'a mut crate::W<REG> {
+        self.variant(SCLKSEL_A::Hext)
+    }
+    #[doc = "System clock from PLL"]
+    #[inline(always)]
+    pub fn pll(self) -> &'a mut crate::W<REG> {
+        self.variant(SCLKSEL_A::Pll)
+    }
+}
 #[doc = "Field `SCLKSTS` reader - System Clock select Status"]
-pub type SCLKSTS_R = crate::FieldReader;
+pub type SCLKSTS_R = crate::FieldReader<SCLKSTS_A>;
+#[doc = "System Clock select Status\n\nValue on reset: 0"]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+#[repr(u8)]
+pub enum SCLKSTS_A {
+    #[doc = "0: System clock from HICK"]
+    Hick = 0,
+    #[doc = "1: System clock from HEXT"]
+    Hext = 1,
+    #[doc = "2: System clock from PLL"]
+    Pll = 2,
+}
+impl From<SCLKSTS_A> for u8 {
+    #[inline(always)]
+    fn from(variant: SCLKSTS_A) -> Self {
+        variant as _
+    }
+}
+impl crate::FieldSpec for SCLKSTS_A {
+    type Ux = u8;
+}
+impl SCLKSTS_R {
+    #[doc = "Get enumerated values variant"]
+    #[inline(always)]
+    pub fn variant(&self) -> Option<SCLKSTS_A> {
+        match self.bits {
+            0 => Some(SCLKSTS_A::Hick),
+            1 => Some(SCLKSTS_A::Hext),
+            2 => Some(SCLKSTS_A::Pll),
+            _ => None,
+        }
+    }
+    #[doc = "System clock from HICK"]
+    #[inline(always)]
+    pub fn is_hick(&self) -> bool {
+        *self == SCLKSTS_A::Hick
+    }
+    #[doc = "System clock from HEXT"]
+    #[inline(always)]
+    pub fn is_hext(&self) -> bool {
+        *self == SCLKSTS_A::Hext
+    }
+    #[doc = "System clock from PLL"]
+    #[inline(always)]
+    pub fn is_pll(&self) -> bool {
+        *self == SCLKSTS_A::Pll
+    }
+}
 #[doc = "Field `AHBDIV` reader - AHB division"]
-pub type AHBDIV_R = crate::FieldReader;
+pub type AHBDIV_R = crate::FieldReader<AHBDIV_A>;
+#[doc = "AHB division\n\nValue on reset: 0"]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+#[repr(u8)]
+pub enum AHBDIV_A {
+    #[doc = "0: The SCLK is used as AHB clock"]
+    Sclk = 0,
+    #[doc = "8: SCLK divided by 2"]
+    Div2 = 8,
+    #[doc = "9: SCLK divided by 4"]
+    Div4 = 9,
+    #[doc = "10: SCLK divided by 8"]
+    Div8 = 10,
+    #[doc = "11: SCLK divided by 16"]
+    Div16 = 11,
+    #[doc = "12: SCLK divided by 64"]
+    Div64 = 12,
+    #[doc = "13: SCLK divided by 128"]
+    Div128 = 13,
+    #[doc = "14: SCLK divided by 256"]
+    Div256 = 14,
+    #[doc = "15: SCLK divided by 512"]
+    Div512 = 15,
+}
+impl From<AHBDIV_A> for u8 {
+    #[inline(always)]
+    fn from(variant: AHBDIV_A) -> Self {
+        variant as _
+    }
+}
+impl crate::FieldSpec for AHBDIV_A {
+    type Ux = u8;
+}
+impl AHBDIV_R {
+    #[doc = "Get enumerated values variant"]
+    #[inline(always)]
+    pub fn variant(&self) -> Option<AHBDIV_A> {
+        match self.bits {
+            0 => Some(AHBDIV_A::Sclk),
+            8 => Some(AHBDIV_A::Div2),
+            9 => Some(AHBDIV_A::Div4),
+            10 => Some(AHBDIV_A::Div8),
+            11 => Some(AHBDIV_A::Div16),
+            12 => Some(AHBDIV_A::Div64),
+            13 => Some(AHBDIV_A::Div128),
+            14 => Some(AHBDIV_A::Div256),
+            15 => Some(AHBDIV_A::Div512),
+            _ => None,
+        }
+    }
+    #[doc = "The SCLK is used as AHB clock"]
+    #[inline(always)]
+    pub fn is_sclk(&self) -> bool {
+        *self == AHBDIV_A::Sclk
+    }
+    #[doc = "SCLK divided by 2"]
+    #[inline(always)]
+    pub fn is_div2(&self) -> bool {
+        *self == AHBDIV_A::Div2
+    }
+    #[doc = "SCLK divided by 4"]
+    #[inline(always)]
+    pub fn is_div4(&self) -> bool {
+        *self == AHBDIV_A::Div4
+    }
+    #[doc = "SCLK divided by 8"]
+    #[inline(always)]
+    pub fn is_div8(&self) -> bool {
+        *self == AHBDIV_A::Div8
+    }
+    #[doc = "SCLK divided by 16"]
+    #[inline(always)]
+    pub fn is_div16(&self) -> bool {
+        *self == AHBDIV_A::Div16
+    }
+    #[doc = "SCLK divided by 64"]
+    #[inline(always)]
+    pub fn is_div64(&self) -> bool {
+        *self == AHBDIV_A::Div64
+    }
+    #[doc = "SCLK divided by 128"]
+    #[inline(always)]
+    pub fn is_div128(&self) -> bool {
+        *self == AHBDIV_A::Div128
+    }
+    #[doc = "SCLK divided by 256"]
+    #[inline(always)]
+    pub fn is_div256(&self) -> bool {
+        *self == AHBDIV_A::Div256
+    }
+    #[doc = "SCLK divided by 512"]
+    #[inline(always)]
+    pub fn is_div512(&self) -> bool {
+        *self == AHBDIV_A::Div512
+    }
+}
 #[doc = "Field `AHBDIV` writer - AHB division"]
-pub type AHBDIV_W<'a, REG, const O: u8> = crate::FieldWriter<'a, REG, 4, O>;
+pub type AHBDIV_W<'a, REG, const O: u8> = crate::FieldWriter<'a, REG, 4, O, AHBDIV_A>;
+impl<'a, REG, const O: u8> AHBDIV_W<'a, REG, O>
+where
+    REG: crate::Writable + crate::RegisterSpec,
+    REG::Ux: From<u8>,
+{
+    #[doc = "The SCLK is used as AHB clock"]
+    #[inline(always)]
+    pub fn sclk(self) -> &'a mut crate::W<REG> {
+        self.variant(AHBDIV_A::Sclk)
+    }
+    #[doc = "SCLK divided by 2"]
+    #[inline(always)]
+    pub fn div2(self) -> &'a mut crate::W<REG> {
+        self.variant(AHBDIV_A::Div2)
+    }
+    #[doc = "SCLK divided by 4"]
+    #[inline(always)]
+    pub fn div4(self) -> &'a mut crate::W<REG> {
+        self.variant(AHBDIV_A::Div4)
+    }
+    #[doc = "SCLK divided by 8"]
+    #[inline(always)]
+    pub fn div8(self) -> &'a mut crate::W<REG> {
+        self.variant(AHBDIV_A::Div8)
+    }
+    #[doc = "SCLK divided by 16"]
+    #[inline(always)]
+    pub fn div16(self) -> &'a mut crate::W<REG> {
+        self.variant(AHBDIV_A::Div16)
+    }
+    #[doc = "SCLK divided by 64"]
+    #[inline(always)]
+    pub fn div64(self) -> &'a mut crate::W<REG> {
+        self.variant(AHBDIV_A::Div64)
+    }
+    #[doc = "SCLK divided by 128"]
+    #[inline(always)]
+    pub fn div128(self) -> &'a mut crate::W<REG> {
+        self.variant(AHBDIV_A::Div128)
+    }
+    #[doc = "SCLK divided by 256"]
+    #[inline(always)]
+    pub fn div256(self) -> &'a mut crate::W<REG> {
+        self.variant(AHBDIV_A::Div256)
+    }
+    #[doc = "SCLK divided by 512"]
+    #[inline(always)]
+    pub fn div512(self) -> &'a mut crate::W<REG> {
+        self.variant(AHBDIV_A::Div512)
+    }
+}
 #[doc = "Field `APB1DIV` reader - APB1 division"]
-pub type APB1DIV_R = crate::FieldReader;
+pub type APB1DIV_R = crate::FieldReader<APB1DIV_A>;
+#[doc = "APB1 division\n\nValue on reset: 0"]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+#[repr(u8)]
+pub enum APB1DIV_A {
+    #[doc = "0: The divided HCLK is used as APB clock"]
+    Hclk = 0,
+    #[doc = "4: HCLK divided by 2"]
+    Div2 = 4,
+    #[doc = "5: HCLK divided by 4"]
+    Div4 = 5,
+    #[doc = "6: HCLK divided by 8"]
+    Div8 = 6,
+    #[doc = "7: HCLK divided by 16"]
+    Div16 = 7,
+}
+impl From<APB1DIV_A> for u8 {
+    #[inline(always)]
+    fn from(variant: APB1DIV_A) -> Self {
+        variant as _
+    }
+}
+impl crate::FieldSpec for APB1DIV_A {
+    type Ux = u8;
+}
+impl APB1DIV_R {
+    #[doc = "Get enumerated values variant"]
+    #[inline(always)]
+    pub fn variant(&self) -> Option<APB1DIV_A> {
+        match self.bits {
+            0 => Some(APB1DIV_A::Hclk),
+            4 => Some(APB1DIV_A::Div2),
+            5 => Some(APB1DIV_A::Div4),
+            6 => Some(APB1DIV_A::Div8),
+            7 => Some(APB1DIV_A::Div16),
+            _ => None,
+        }
+    }
+    #[doc = "The divided HCLK is used as APB clock"]
+    #[inline(always)]
+    pub fn is_hclk(&self) -> bool {
+        *self == APB1DIV_A::Hclk
+    }
+    #[doc = "HCLK divided by 2"]
+    #[inline(always)]
+    pub fn is_div2(&self) -> bool {
+        *self == APB1DIV_A::Div2
+    }
+    #[doc = "HCLK divided by 4"]
+    #[inline(always)]
+    pub fn is_div4(&self) -> bool {
+        *self == APB1DIV_A::Div4
+    }
+    #[doc = "HCLK divided by 8"]
+    #[inline(always)]
+    pub fn is_div8(&self) -> bool {
+        *self == APB1DIV_A::Div8
+    }
+    #[doc = "HCLK divided by 16"]
+    #[inline(always)]
+    pub fn is_div16(&self) -> bool {
+        *self == APB1DIV_A::Div16
+    }
+}
 #[doc = "Field `APB1DIV` writer - APB1 division"]
-pub type APB1DIV_W<'a, REG, const O: u8> = crate::FieldWriter<'a, REG, 3, O>;
+pub type APB1DIV_W<'a, REG, const O: u8> = crate::FieldWriter<'a, REG, 3, O, APB1DIV_A>;
+impl<'a, REG, const O: u8> APB1DIV_W<'a, REG, O>
+where
+    REG: crate::Writable + crate::RegisterSpec,
+    REG::Ux: From<u8>,
+{
+    #[doc = "The divided HCLK is used as APB clock"]
+    #[inline(always)]
+    pub fn hclk(self) -> &'a mut crate::W<REG> {
+        self.variant(APB1DIV_A::Hclk)
+    }
+    #[doc = "HCLK divided by 2"]
+    #[inline(always)]
+    pub fn div2(self) -> &'a mut crate::W<REG> {
+        self.variant(APB1DIV_A::Div2)
+    }
+    #[doc = "HCLK divided by 4"]
+    #[inline(always)]
+    pub fn div4(self) -> &'a mut crate::W<REG> {
+        self.variant(APB1DIV_A::Div4)
+    }
+    #[doc = "HCLK divided by 8"]
+    #[inline(always)]
+    pub fn div8(self) -> &'a mut crate::W<REG> {
+        self.variant(APB1DIV_A::Div8)
+    }
+    #[doc = "HCLK divided by 16"]
+    #[inline(always)]
+    pub fn div16(self) -> &'a mut crate::W<REG> {
+        self.variant(APB1DIV_A::Div16)
+    }
+}
 #[doc = "Field `APB2DIV` reader - APB2 division"]
-pub type APB2DIV_R = crate::FieldReader;
+pub use APB1DIV_R as APB2DIV_R;
 #[doc = "Field `APB2DIV` writer - APB2 division"]
-pub type APB2DIV_W<'a, REG, const O: u8> = crate::FieldWriter<'a, REG, 3, O>;
+pub use APB1DIV_W as APB2DIV_W;
 #[doc = "Field `ADCDIV1_0` reader - ADC division bit1 and bit0"]
 pub type ADCDIV1_0_R = crate::FieldReader;
 #[doc = "Field `ADCDIV1_0` writer - ADC division bit1 and bit0"]
